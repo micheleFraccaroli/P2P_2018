99310ad40e48e8e9048fd84ffe398c26cd15fa14
Matteo Gemelli <matteogemelli5@gmail.com>
2018-03-27 18:15:07 +0200

    Receive funzionante all 90 %

--- Distribuited_directory/Receive.py
+++ Distribuited_directory/Receive.py
@@ -1,10 +1,16 @@
 import socket
+from pathlib import Path
+import glob
 import Util
 import time
 import datetime
+import re
+import os
 import ipaddress as ipaddr
+import hashlib
 import multiprocessing as mp
 from dataBase import dataBase
+from Config import Config
 
 class Receive():
 	def __init__(self, my_ip, my_door):
@@ -28,14 +34,28 @@ class Receive():
 		print(pktid_dict[str(key)])
 		return pktid_dict
 
-	#mette in ascolto una socket in ipv4 e in ipv6, pronta per ricevere le richieste
-	#quando arriva una richiesta, viene controllato se il pachet id è già stato salvato nel dizzionario
-	#nel caso positivo verifico se sono trascorsi 300s
-	#se sono trascorsi, cancello la voce dal dizzionario e invio le richieste ai peer vicini, 
-	#in caso contrario non faccio nulla, evitando così la presenza di richieste rindondanti nella rete
+	#calcolo l'md5 dei file della ricerca
+
+	def convert_md5(self, file_list):
+		dict_list = {}
+		for file in file_list:
+			open_file = open('img/'+file, 'rb')
+			contenuto = open_file.read()
+			FileHash = hashlib.md5()
+			FileHash.update(contenuto)
+			dict_list[file] = FileHash.hexdigest()
+		return dict_list
+
+	#risponde al peer che ha effettuato una ricerca
+
+	def answer(self, file_list, pktid, ip, door, other_peersocket):
+		dict_list = self.convert_md5(file_list)
+		for file in dict_list:
+			file_complete = file.ljust(100,' ')
+			answer = "AQUE"+pktid+ip+door+dict_list[file]+file_complete
+			other_peersocket.send(answer.encode())
 
 	def listen_other(self):
-		global pktid_list
 		if(str(ipaddr.ip_address(self.my_ip)).find('.') != -1): #ha trovato il punto, quindi ipv4
 			peersocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			peersocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -50,7 +70,6 @@ class Receive():
 		peersocket.listen(5)
 
 		while True:
-			print('sono in ascolto\n')
 			other_peersocket, addr = peersocket.accept()
 			self.from_peer = other_peersocket.recv(102)
 			self.bytes_read = len(self.from_peer)
@@ -60,13 +79,28 @@ class Receive():
 				self.bytes_read = len(self.from_peer)
 
 			if(self.from_peer[0:4].decode() == "QUER"):
+
 				self.pktid = self.from_peer[4:16].decode()
 				self.ip  = self.from_peer[16:75].decode()
+				self.door = self.from_peer[75:80].decode()
+				self.ttl = self.from_peer[80:82].decode()
+				self.string = self.from_peer[82:].decode().rstrip()
 
 				db = dataBase()
 				res = db.retrivenSearch(self.pktid, self.ip)
 
 				if(res == 0):
+					print("vado a verificare se possiedo il file indicato nella ricerca\n")
+
+					file_found = []
+
+					for file in os.listdir("img"):
+						if re.search(self.string, file, re.IGNORECASE):
+							file_found.append(file)
+
+					if(len(file_found) != 0):
+						self.answer(file_found, self.pktid, self.ip, self.door, other_peersocket)
+
 					print("vado ad aggiungere un nuovo record al dizzionario\n")
 					self.timestamp = time.time()
 					db.insertSearch(self.pktid, self.ip, self.timestamp)
@@ -78,19 +112,17 @@ class Receive():
 					before = db.retriveSearch(self.pktid, self.ip)
 					now = time.time()
 					if((now - before) < 300):
-						print('non faccio nulla\n')
+						print('non faccio nulla perchè ho già elaborato la richiesta\n')
 					else:
 						print('andrò ad aggiornare nel db il timestamp\n')
 					del db 
 
-'''
-def answer(self):
 
-'''
 if __name__ == '__main__':
 
 	db = dataBase()
-	db.create()
+	conf = Config()
+	db.create(conf)
 	del db
 
 	Receive_4 = Receive('127.0.0.1','50004')
