4967433ae12fe24ef1cc5d094554ca6a9ae61454
Michele <michelefraccaroli@gmail.com>
2018-03-29 09:32:49 +0200

    working to main

--- Distribuited_directory/main.py
+++ Distribuited_directory/main.py
@@ -1,7 +1,7 @@
 import Util
 import time
 from Config import Config
-from time import *
+#from time import *
 from Vicini import Vicini
 from Ricerca import Ricerca
 from Retr import Retr
@@ -9,7 +9,7 @@ from dataBase import dataBase
 from tqdm import tqdm
 from Download import Download
 
-
+print("\n")
 print("____________________________ ")
 print("\______   \_____  \______   \ ")
 print(" |     ___//  ____/|     ___/")
@@ -19,8 +19,8 @@ print("                  \/         \n")
 
 
 print('Loading configuration file')
-for i in tqdm(range(4), desc="Loading: "):
-	time.sleep(1)
+for i in tqdm(range(3), desc="Loading: "):
+    time.sleep(1)
 print("Configuration done!")
 
 c=Config() #istanza delle configurazioni
@@ -39,27 +39,26 @@ print("\n------------------------\n")
 while True:
     name_search = input("Insert file to search into net: ")
     search = Ricerca(c.selfV4, c.selfV6, c.selfP, c.ttl, c.timeResearch, name_search)
-    print("\n--- New research launched ---> pid: " + pktid + "\n")
     pktid = search.query(c)
+    print("\n--- New research launched ---\n")
 
     for i in tqdm(range(35), desc="Loading: "):
-    	time.sleep(1)
+        time.sleep(1)
 
-	# choice section
-	print("Research termined\n--- Result for " + pktid + " ---")
-	print("[select with the number the file to download")
-	res = db.retrieveResponses(pktid)
-	choice_list = []
-	i = 1
-	for row in res:
-		print(i + ") " + "ip: " + row[1] + "  port: " + row[2] + "  md5: " + row[3] + "  file: " + row[4])
-		choice_list.append(row)
-		i = i + 1
+    print("Research termined\n--- Result for " + pktid + " ---")
+    print("[select with the number the file to download")
+    res = db.retrieveResponses(pktid)
+    choice_list = []
+    i = 1
+    for row in res:
+        print(i + ") " + "ip: " + row[1] + "  port: " + row[2] + "  md5: " + row[3] + "  file: " + row[4])
+        choice_list.append(row)
+        i = i + 1
 
-	choice = input("> ")
-		peer = choice_list[choice-1]
-		ipv4,ipv6 = peer[1].split('|')
-		download = Download(ipv4,ipv6,peer[2],peer[2],peer[4])
-		download.download()
+    choice = input("> ")
+    peer = choice_list[choice-1]
+    ipv4,ipv6 = peer[1].split('|')
+    download = Download(ipv4,ipv6,peer[2],peer[2],peer[4])
+    download.download()
 
 del db
