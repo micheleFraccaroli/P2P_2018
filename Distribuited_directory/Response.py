1674b69ea0621b3b93782ea35dd891df380cafa2
Michele <michelefraccaroli@gmail.com>
2018-04-10 09:53:47 +0200

    Chiusi i thread per ogni risposta

--- Distribuited_directory/Response.py
+++ Distribuited_directory/Response.py
@@ -18,22 +18,37 @@ class thread_Response(th.Thread):
         th.Thread.__init__(self) # thread istance second level
         self.bytes_read = 0
         self.other_peersocket = other_peersocket
+        self.other_peersocket.setblocking(0)
         self.lock = lock
 
     def run(self):
         db = dataBase()
-        #while True:
-        Util.printLog("THREAD SECONDO LIVELLO -----> RESPONSE")
-        recv_packet = self.other_peersocket.recv(212)
+        while True:
+            try:
+                Util.printLog("--------INIZIO LETTURA SECONDO LIVELLO-------")
+                recv_packet = self.other_peersocket.recv(212)
+                Util.printLog("--------FINE LETTURA SECONDO LIVELLO ---------")
+            except:
+                Util.printLog("NIENTE DA LEGGERE LIVELLO DUE.. ESCO E TORNO A LIV UNO" + str(self.other_peersocket))
+                sys.exit()
 
-        self.bytes_read = len(recv_packet)
-        while(self.bytes_read < 212):
-            recv_packet += self.other_peersocket.recv(212 - self.bytes_read)
+            Util.printLog("THREAD SECONDO LIVELLO -----> RESPONSE")
             self.bytes_read = len(recv_packet)
 
-        # retrieving data from file research
-        if(recv_packet[:4].decode() == "AQUE"):
-            self.lock.acquire()
-            db.insertResponse(recv_packet[4:20].decode(), recv_packet[20:75].decode(), recv_packet[75:80].decode(), recv_packet[80:112].decode(), recv_packet[112:212].decode())
-            self.lock.release()
+            if(self.bytes_read == 0):
+                Util.printLog("ESCO DAL SECONDO LIVELLO DAVVERO")
+                sys.exit()
+
+            while(self.bytes_read < 212):
+                Util.printLog(str(self.bytes_read))
+                recv_packet += self.other_peersocket.recv(212 - self.bytes_read)
+                self.bytes_read = len(recv_packet)
+
+            Util.printLog(str(recv_packet.decode()))
+
+            # retrieving data from file research
+            if(recv_packet[:4].decode() == "AQUE"):
+                self.lock.acquire()
+                db.insertResponse(recv_packet[4:20].decode(), recv_packet[20:75].decode(), recv_packet[75:80].decode(), recv_packet[80:112].decode(), recv_packet[112:212].decode())
+                self.lock.release()
         Util.printLog("CHIUDO THREAD LIVELLO DUE")
