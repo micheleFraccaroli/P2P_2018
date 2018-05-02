b9a6f4d106f779f2ca328e4f67396ed428bdc91a
Gemelli Matteo <matteogemelli5@gmail.com>
2018-05-02 12:48:47 +0200

    Modifiche in fase di debug

--- Hierarchical_directory/Download.py
+++ Hierarchical_directory/Download.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from Conn import Conn
 import multiprocessing as mp
 import Util
+from curses import *
 
 class Download:
     def __init__(self, ipp2p_B_4, ipp2p_B_6, pp2p_B, md5, filename):
@@ -70,7 +71,7 @@ class Download:
         check_file = Path(path_file)
 
         if (check_file.is_file()):
-            
+
             res = wrapper(Util.menu,['Yes',True,'No',False],['The file requested already exists. Override it?'])
 
             if res:
@@ -101,7 +102,7 @@ class Download:
         Util.waitMenu.acquire()
         Util.waitMenu.notify()
         Util.waitMenu.release()
-        
+
 '''
 if __name__ == "__main__":
     l = []
@@ -112,5 +113,5 @@ if __name__ == "__main__":
     op = input("'D' for download: ")
     if (op == "D"):
         down.download()
-        print("\n--- END ---") 
+        print("\n--- END ---")
 '''
