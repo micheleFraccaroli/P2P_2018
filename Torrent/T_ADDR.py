f97e311f5ecc98db95ccf43166d136a9882be060
Gemelli Matteo <matteogemelli5@gmail.com>
2018-05-10 17:17:49 +0200

    creazione thread per la gestione della richiesta di ADDR

---
+++ Torrent/T_ADDR.py
@@ -0,0 +1,37 @@
+import socket
+import Util
+import sys
+import os
+import ipaddress as ipad
+from dataBase import dataBase
+from Conn import Conn
+import threading as th
+
+#thread che si occupa della gestione dell'aggiunta di un file proveniente da un peer
+
+class T_ADDR(th.Thread):
+    def __init__(self, socket):
+        th.Thread.__init__(self)
+        self.socket = socket
+
+    def run(self):
+        self.addr_pkt = self.socket.recv(164)
+
+        if(len(self.addr_pkt)!=0):
+            self.bytes_read = len(self.addr_pkt)
+            while(self.bytes_read < 164):
+                self.addr_pkt += self.socket.recv(164-self.bytes_read)
+                self.bytes_read = len(self.addr_pkt)
+
+        self.sessionid = self.addr_pkt[4:20]
+        self.lenfile = self.addr_pkt[20:30]
+        self.lenpart = self.addr_pkt[30:36]
+        self.filename = self.addr_pkt[36:136]
+        self.md5 = self.addr_pkt[136:]
+
+        db = dataBase()
+        npart = db.insert_file(self.sessionid, self.md5, self.filename, self.lenfile, self.lenpart)
+        self.aadr_pkt = "AADR"+npart
+
+        self.socket.send(self.addr_pkt.encode())
+        self.socket.close()
--- Torrent/dataBase.py
+++ Torrent/dataBase.py
@@ -1,4 +1,5 @@
 import sqlite3 as s3
+import math
 
 class dataBase:
 	def __init__(self):
@@ -8,7 +9,7 @@ class dataBase:
 			c = con.cursor()
 
 			c.execute('CREATE TABLE IF NOT EXISTS login (ip VARCHAR(55) NOT NULL, port VARCHAR(5) NOT NULL, idSession VARCHAR(16) NOT NULL,PRIMARY KEY(ip))')
-
+			c.execute('CREATE TABLE IF NOT EXISTS file (sessionid VARCHAR(16) NOT NULL, md5 VARCHAR(32) NOT NULL, name VARCHAR(100), lenfile INTEGER(10), lenpart INTEGER(6), npart INTEGER(8),PRIMARY KEY(sessionid, md5))')
 			con.commit()
 			con.close()
 
@@ -21,4 +22,13 @@ class dataBase:
 			con.commit()
 			con.close()
 
-		
+		def insert_file(self, sessionid, md5, name, lenfile, lenpart):
+			con = s3.connect('TorrentDB.db')
+			c = con.cursor()
+			npart = math.ceil((lenfile/lenpart))
+
+			res = c.execute('INSERT INTO file VALUES(?,?,?,?,?,?)', (sessionid, md5, name, lenfile, lenpart, npart))
+
+			con.commit()
+			con.close()
+			return str(npart)
