e47ab153ead4df41b62e5ecc924cd75d0ea3fff6
Gemelli Matteo <matteogemelli5@gmail.com>
2018-05-14 16:32:00 +0200

    sviluppo look

--- Torrent/dataBase.py
+++ Torrent/dataBase.py
@@ -2,68 +2,67 @@ import sqlite3 as s3
 import math
 
 class dataBase:
-	def __init__(self):
 
-		def create(self):
-			con = s3.connect('TorrentDB.db')
-			c = con.cursor()
+	def create(self):
+		con = s3.connect('TorrentDB.db')
+		c = con.cursor()
 
-			c.execute('CREATE TABLE IF NOT EXISTS login (ip VARCHAR(55) NOT NULL, port VARCHAR(5) NOT NULL, idSession VARCHAR(16) NOT NULL,PRIMARY KEY(ip))')
-			c.execute('CREATE TABLE IF NOT EXISTS file (sessionid VARCHAR(16) NOT NULL, md5 VARCHAR(32) NOT NULL, name VARCHAR(100), lenfile VARCHAR(10), lenpart VARCHAR(6), npart VARCHAR(8),PRIMARY KEY(sessionid, md5))')
-			con.commit()
-			con.close()
+		c.execute('CREATE TABLE IF NOT EXISTS login (ip VARCHAR(55) NOT NULL, port VARCHAR(5) NOT NULL, idSession VARCHAR(16) NOT NULL,PRIMARY KEY(ip))')
+		c.execute('CREATE TABLE IF NOT EXISTS file (sessionid VARCHAR(16) NOT NULL, md5 VARCHAR(32) NOT NULL, name VARCHAR(100), lenfile INTEGER, lenpart INTEGER, npart INTEGER,PRIMARY KEY(sessionid, md5))')
+		con.commit()
+		con.close()
 
-		def login(self, ip, port, sid):
-			con = s3.connect('TorrentDB.db')
-			c = con.cursor()
+	def login(self, ip, port, sid):
+		con = s3.connect('TorrentDB.db')
+		c = con.cursor()
 
-			res = c.execute('INSERT INTO login VALUES (?,?,?)', (ip, port, sid))
+		res = c.execute('INSERT INTO login VALUES (?,?,?)', (ip, port, sid))
 
-			con.commit()
-			con.close()
+		con.commit()
+		con.close()
 
-		def insert_file(self, sessionid, md5, name, lenfile, lenpart):
-			con = s3.connect('TorrentDB.db')
-			c = con.cursor()
-			npart = math.ceil((lenfile/lenpart))
+	def insert_file(self, sessionid, md5, name, lenfile, lenpart):
+		con = s3.connect('TorrentDB.db')
+		c = con.cursor()
+		npart = math.ceil((lenfile/lenpart))
 
-			c.execute('INSERT INTO file VALUES(?,?,?,?,?,?)', (sessionid, md5, name, lenfile, lenpart, str(npart).zfill(8)))
+		c.execute('INSERT INTO file VALUES(?,?,?,?,?,?)', (sessionid, md5, name, lenfile, lenpart, npart))
 
-			con.commit()
-			con.close()
-			return str(npart).zfill(8)
+		con.commit()
+		con.close()
+		return str(npart).zfill(8)
 
-		def check_file(self, sessionid, md5):
-			con = s3.connect('TorrentDB.db')
-			c = con.cursor()
+	def check_file(self, sessionid, md5):
+		con = s3.connect('TorrentDB.db')
+		c = con.cursor()
 
-			c.execute('SELECT count(*) FROM file WHERE sessionid=? AND md5=?',(sessionid, md5))
-			res = c.fetchone()
+		c.execute('SELECT count(*) FROM file WHERE sessionid=? AND md5=?',(sessionid, md5))
+		res = c.fetchone()
 
-			con.commit()
-			con.close()
-			return res[0]
+		con.commit()
+		con.close()
+		return res[0]
 
-		def update_file(self, sessionid, md5, name, lenfile, lenpart):
-			con = s3.connect('TorrentDB.db')
-			c = con.cursor()
-			npart = math.ceil((lenfile/lenpart))
+	def update_file(self, sessionid, md5, name, lenfile, lenpart):
+		con = s3.connect('TorrentDB.db')
+		c = con.cursor()
+		npart = math.ceil((lenfile/lenpart))
 
-			c.execute('UPDATE file SET name = ?, lenfile = ?, lenpart = ?, npart = ?,  WHERE md5 = ? AND sessionid = ?', (name, lenfile, lenpart, npart, md5, sessionid))
+		c.execute('UPDATE file SET name = ?, lenfile = ?, lenpart = ?, npart = ?  WHERE md5 = ? AND sessionid = ?', (name, lenfile, lenpart, npart, md5, sessionid))
 
-			con.commit()
-			con.close()
-			return str(npart).zfill(8)
+		con.commit()
+		con.close()
+		return str(npart).zfill(8)
 
-		def search_files(self, string):
-			con = s3.connect('TorrentDB.db')
-			c = con.cursor()
+	def search_files(self, string):
+		con = s3.connect('TorrentDB.db')
+		c = con.cursor()
 
-			c.execute('SELECT count(DISTINCT md5) FROM file WHERE name LIKE ?', ('%'+string.strip()+'%',))
-			nmd5 = c.fetchone()
+		c.execute('SELECT count(DISTINCT md5) FROM file WHERE name LIKE ?', ('%'+string.strip()+'%',))
+		nmd5 = c.fetchone()
 
-			c.execute('SELECT md5, name, lenfile, lenpart FROM file WHERE name LIKE ? ORDER BY md5', ('%'+string.strip()+'%',))
-			file = c.fetchall()
+		c.execute('SELECT md5, name, lenfile, lenpart FROM file WHERE name LIKE ? ORDER BY md5', ('%'+string.strip()+'%',))
+		file = c.fetchall()
 
-			con.close()
-			return nmd5[0], file
+		con.close()
+		return nmd5[0], file
--- Torrent/t_look.py
+++ Torrent/t_look.py
@@ -7,7 +7,7 @@ from dataBase import dataBase
 from Conn import Conn
 import threading as th
 
-class t_aloo(th.Thread):
+class t_look(th.Thread):
     def __init__(self, socket):
         th.Thread.__init__(self)
         self.socket = socket
@@ -21,8 +21,9 @@ class t_aloo(th.Thread):
                 self.look_pkt += self.socket.recv(36-self.bytes_read)
                 self.bytes_read = len(self.look_pkt)
 
-        self.search = self.look_pkt[20:].decode()
+        self.search = self.look_pkt[16:].decode()
         db = dataBase()
+        db.create()
         nmd5, res = db.search_files(self.search)
 
         self.aloo_pkt = "ALOO"+str(nmd5).zfill(3)
@@ -34,3 +35,15 @@ class t_aloo(th.Thread):
                 self.socket.send(self.aloo_pkt.encode())
 
         self.socket.close()
+
+if __name__ == '__main__':
+
+    peersocket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    peersocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    peersocket.bind(('', 3000))
+
+    peersocket.listen(20)
+    other_peersocket, addr = peersocket.accept()
+
+    th_addr =  T_ADDR(other_peersocket)
+    th_addr.start()
